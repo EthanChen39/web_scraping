155a1c659fbe0ee664d3e8bb9e67437195c729e4
EthanChen39 <1014317748123qwe@gmail.com>
2020-04-20 18:31:23 -0700

    first commit

---
+++ transcript.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+loginURL = 'https://swing.langara.bc.ca/prod/twbkwbis.P_WWWLogin'
+# browser = webdriver.Chrome(executable_path='/Users/ethanchen/PycharmProjects/web_scraping/chromedriver')
+browser = webdriver.Safari()
+browser.get(loginURL)
+studentID = browser.find_element_by_id('UserID')
+studentID.send_keys('100304065')
+time.sleep(0.3)
+password = browser.find_element_by_name('PIN')
+password.send_keys('SC-20000309')
+time.sleep(0.3)
+password.send_keys(Keys.ENTER)
+time.sleep(1.3)
+transcriptURL = 'https://swing.langara.bc.ca/prod/bwskotrn.P_ViewTran'
+browser.get(transcriptURL)
+
+
+
+
+
+
